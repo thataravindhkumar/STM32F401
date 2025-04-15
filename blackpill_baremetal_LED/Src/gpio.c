b8241c1841afc423a13e3ebfb894cacf21de8940
thataravindhkumar <thataravindhkumar@gmail.com>
2025-04-15 16:21:12 +0530

    Minor changes in comments to make it more readable.

--- blackpill_baremetal_LED/Src/gpio.c
+++ blackpill_baremetal_LED/Src/gpio.c
@@ -2,9 +2,9 @@
 
 void gpio_init(void) //Initializing GPIO pin(s)
 {
-	RCC_AHB1ENR |= (1U<<2); //Bitwise OR operation to make bit 2 to become 1, enabling RCC for GPIOC
+	RCC_AHB1ENR |= (1U<<2); //Bitwise OR operation to set bit 2 to 1, enabling RCC for GPIOC
 
-	//Setting "01" to MODE13[1:0] to set PC13 (user LED) as GPIO output
+	//Writing "01" at MODE13[1:0] to set PC13 (user LED) as GPIO output
 	GPIOC_MODER |= (1U<<26); //Setting 26th bit of GPIOC_MODER register to 1
 	GPIOC_MODER &= ~(1U<<27); //Setting 27th bit of GPIOC_MODER register to 0
 }
